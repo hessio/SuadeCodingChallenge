77500cc3d6a4752e58c6673fc05dbfe48c7b719d
hessio <patrickhession97@gmail.com>
2023-08-01 21:59:08 +0100

    removed unnecessary database configurations

--- config.py
+++ config.py
@@ -1,6 +1,3 @@
 class Config:
     DEBUG = True
     SECRET_KEY = "random-key-word"
-    # MySQL Database Config
-    SQLALCHEMY_DATABASE_URI = 'mysql://root:Dangermouse180@localhost/user_database'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
